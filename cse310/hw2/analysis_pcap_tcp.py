2bcf30df6ef6e4030095e3577cfba766c0a6c2cf
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-04-03 21:19:16 -0400

    Add: More work on keeping packets

--- cse310/hw2/analysis_pcap_tcp.py
+++ cse310/hw2/analysis_pcap_tcp.py
@@ -1,6 +1,32 @@
 import dpkt
 import socket
+import heapq
 from enum import Enum
+
+
+class Comp:
+    def __init__(self, val):
+        self.val = val
+
+    def __lt__(self, other):
+        return abs(self.val - target) < abs(other.val - target)
+
+
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def push(self, val):
+        heapq.heappush(self.heap, Comp(val))
+
+    def pop(self):
+        return heapq.heappop(self.heap).val
+
+    def peek(self):
+        return self.heap[0].val if self.heap else None
+
+    def __len__(self):
+        return len(self.heap)
 
 
 class STATE(Enum):
@@ -15,6 +41,10 @@ class STATE(Enum):
     RECEIVED_FIN_ACK = 8
     ENDING = 9
 
+class Packet:
+
+    def __init__(self, seq, ack, conn_key):
+        self.seq = 
 
 class Flow:
     def __init__(self, conn_key, ip, tcp, start):
@@ -41,9 +71,11 @@ f = open('assignment2.pcap', 'rb')
 pcap = dpkt.pcap.Reader(f)
 
 flows = dict()
+one_flow = None
 time = -1
+MAX = 10000000000
 PACKET_COUNT = 0
-PACKET_CONTROL = 7
+PACKET_CONTROL = 10
 for timestamp, buf in pcap:
     if PACKET_COUNT == PACKET_CONTROL:
         break
@@ -86,34 +118,44 @@ for timestamp, buf in pcap:
             # Maybe the other person can initiate FIN?
             if curr_packet.state == STATE.IN_PROGRESS and conn_key in flows.keys():
                 curr_packet.state = STATE.SENT_FIN
-            print("FIN_SENT")
         elif tcp.flags & dpkt.tcp.TH_ACK:
             if curr_packet.state == STATE.RECEIVED_SYN_ACK:
                 curr_packet.state = STATE.SENT_SECOND_SYN
             if curr_packet.state == STATE.SENT_SECOND_SYN and conn_key in flows.keys():
                 curr_packet.state = STATE.FIRST_TRANSACTION_SENT
                 # This packet is sending the first transaction
-                print("FIRST TRANSACTION")
-                print(conn_key)
+                # print("FIRST TRANSACTION")
+                # print(conn_key)
                 sequence_number = tcp.seq
                 ack_number = tcp.ack
                 window_size = tcp.win
-                print("Sequence number:", sequence_number)
-                print("Ack number:", ack_number)
-                print("Receive Window size:", window_size)
-                print("-------------------------------------")
+                # print("Sequence number:", sequence_number)
+                # print("Ack number:", ack_number)
+                # print("Receive Window size:", window_size)
+                # print("-------------------------------------")
+            if conn_key in flows.keys():
+                sequence_number = tcp.seq
+                ack_number = tcp.ack
+                if sequence_number not in curr_packet.acks_seqs.keys():
+                    curr_packet.acks_seqs.update({sequence_number: 1})
+                    # Add a ACK that we are looking for
+            else:
+                sequence_number = tcp.seq
+                ack_number = tcp.ack
+                if ack_number in curr_packet.acks_seqs.keys():
+                    curr_packet.acks_seqs[ack_number] -= 1
             if curr_packet.state == STATE.FIRST_TRANSACTION_SENT and conn_key in flows.keys():
                 curr_packet.state = STATE.SECOND_TRANSACTION_SENT
                 # This packet is sending the second transaction
-                print("SECOND TRANSACTION")
-                print(conn_key)
+                # print("SECOND TRANSACTION")
+                # print(conn_key)
                 sequence_number = tcp.seq
                 ack_number = tcp.ack
                 window_size = tcp.win
-                print("Sequence number:", sequence_number)
-                print("Ack number:", ack_number)
-                print("Receive Window size:", window_size)
-                print("-------------------------------------")
+                # print("Sequence number:", sequence_number)
+                # print("Ack number:", ack_number)
+                # print("Receive Window size:", window_size)
+                # print("-------------------------------------")
             if curr_packet.state == STATE.SECOND_TRANSACTION_SENT and conn_key in flows.keys():
                 curr_packet.state = STATE.IN_PROGRESS
             if curr_packet.state == STATE.RECEIVED_FIN_ACK and conn_key in flows.keys():
@@ -122,27 +164,31 @@ for timestamp, buf in pcap:
             curr_packet.finish_time = timestamp - time
             if curr_packet.state == STATE.IN_PROGRESS and conn_key in flows.keys():
                 curr_packet.state = STATE.SENT_FIN
-
+        if one_flow == None:
+            one_flow = conn_key
         # Want to check from sender to reciever
         # Want to see how many repeat we get
-        if conn_key in flows.keys():
-            ack_num =ip.data.ack
+        if conn_key in flows.keys() and (one_flow == conn_key):
+            ack_num = ip.data.ack
             seq_num = ip.data.seq
-            print("ACK + Seq SENT")
-            print(ack_num)
-            print(seq_num)
+            print("SENT")
+            print("ACK: " + str(ack_num))
+            print("SEQ: " + str(seq_num))
             print("----------------------------")
-        elif conn_key_reverse in flows.keys():
-            ack_num =ip.data.ack
+        elif conn_key_reverse in flows.keys() and (one_flow == conn_key_reverse):
+            ack_num = ip.data.ack
             seq_num = ip.data.seq
-            print("ACK + Seq RECV")
-            print(ack_num)
-            print(seq_num)
+            print("RECV")
+            print("ACK: " + str(ack_num))
+            print("SEQ: " + str(seq_num))
             print("----------------------------")
         # print(f"Protocol: {protocol}, Source IP: {src_ip}, Source Port: {src_port}, Destination IP: {dst_ip}, Destination Port: {dst_port}, Flags: {state}")
 for flow_key in flows.keys():
     print("Connection: (src_ip, src_port, dest_ip, dest_port)")
-    print(flow_key)
+    curr_flow = flows[flow_key]
+    for key in curr_flow.acks_seqs.keys():
+        if curr_flow.acks_seqs[key] > 0:
+            print(key)
     print("Throughput (bytes per second)")
     # print(flows[flow_key].get_throughput())
     print("--------------------------------")
