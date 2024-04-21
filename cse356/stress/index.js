dfe1b32aec026be31dd094d89bdb07aea3325e11
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-04-20 23:45:35 -0400

    More work on setting up logging of metrics

--- cse356/stress/index.js
+++ cse356/stress/index.js
@@ -16,18 +16,22 @@ export const options = {
     scenarios: {
         constant_load: {
           executor: 'constant-arrival-rate',
-          rate: 4000, // 4000 requests per second
+          rate: 1000, // 4000 requests per second
           timeUnit: '1s', // adjust this if you want to run for a different duration
-          duration: '30s', // adjust this if you want to run for a different duration
+          duration: '20s', // adjust this if you want to run for a different duration
           preAllocatedVUs: 1000, // adjust this based on your desired concurrency
           maxVUs: 8000, // adjust this based on your desired max concurrency
         },
       },
+      http: {
+        timeout: '2s', // Adjust the timeout value as needed
+    },
 };
 
 export default () => {
-    let x = Math.floor(Math.random() * (35000 - 30000 + 1)) + 30000;
-    let y = Math.floor(Math.random() * (49000 - 44000 + 1)) + 44000;
+    let x = Math.floor(Math.random() * (30050 - 30000 + 1)) + 30000;
+    let y = Math.floor(Math.random() * (44050 - 44000 + 1)) + 44000;
     const urlRes = http.get(`http://143.198.19.157/tiles/17/${x}/${y}.png`);
+    //const urlRes = http.get(`http://146.190.141.61:8080/tile/17/${x}/${y}.png`);
     sleep(1);
 };
