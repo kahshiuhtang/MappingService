6bbcb6fe8a525e722ec35b4b75e59c23d46bc78c
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-04-21 17:39:30 -0400

    work on fixing search

--- cse356/setup/search1.js
+++ cse356/setup/search1.js
@@ -27,6 +27,16 @@ app.post("/api/search", async function (req, res) {
             place: searchTerm,
           },
         },
+      ]
+     }
+    },
+    size: 1000,
+  };
+  var query1 = {
+    index: idx,
+    query: {
+     bool:{
+      must: [
         {
           match: {
             other_tags: searchTerm,
@@ -37,13 +47,41 @@ app.post("/api/search", async function (req, res) {
     },
     size: 1000,
   };
+  
   if(onlyInBox){
     query = {
-      index: idx,
-      query: {
-       bool:{
-        must: [
-          {
+        index: idx,
+        query: {
+         bool:{
+          should: [
+            {
+              match: {
+                name: searchTerm,
+              },
+            },
+            {
+              match: {
+                place: searchTerm,
+              },
+            },
+          ]
+         }
+        },
+        size: 1000,
+      };
+      query1 = {
+        index: idx,
+        query: {
+         bool:{
+          must: [
+            {
+              match: {
+                other_tags: searchTerm,
+              },
+            },
+            
+          ],
+          filter: {
             geo_bounding_box: {
               geometry: {
                 top_left: {
@@ -57,23 +95,39 @@ app.post("/api/search", async function (req, res) {
               }
             }
           }
-        ],
-        should: [
-          {
-            match: {
-              name: searchTerm,
-            },
-          },
-          {
-            match: {
-              place: searchTerm,
-            },
+         }
+        },
+        size: 1000,
+      };
+    query1 = {
+        index: idx,
+        query: {
+         bool:{
+          should: [
+            {
+                match: {
+                  other_tags: searchTerm,
+                },
+              },
+          ],
+          filter: {
+            geo_bounding_box: {
+              geometry: {
+                top_left: {
+                  "lat": maxLat,
+                  "lon": minLon
+                },
+                bottom_right: {
+                  "lat": minLat,
+                  "lon": maxLon
+                }
+              }
+            }
           }
-        ]
-       }
-      },
-      size: 1000,
-    }
+         }
+        },
+        size: 1000,
+      }
   }
   var idx = "points,lines,multilinestrings,multipolygons,other_relations";
   function hstoreToJson(hstoreString) {
@@ -90,16 +144,19 @@ app.post("/api/search", async function (req, res) {
 }
   try {
     const result = await client.search(query);
+    const result1 = await client.search(query1);
     const results = [];
     var rest = result.hits.hits;
-    for (var i = 0; i < rest.length; i++) {
-      const curr = rest[i];
-      var coords = curr["_source"]["geometry"]["coordinates"];
-      var name = "";
-      if(curr["_source"]["name"]){
-        name = curr["_source"]["name"]
-      }else{
-        row = hstoreToJson(curr["_source"]["other_tags"])
+    var rest1  =result1.hits.hits;
+    for (var i = 0; i < rest1.length; i++) {
+        const curr = rest1[i];
+        var coords = curr["_source"]["geometry"]["coordinates"];
+        var name = "";
+        if(!curr["_source"]["other_tags"]){
+            continue;
+        }
+	    row = hstoreToJson(curr["_source"]["other_tags"])
+        console.log(row)
         name += row["addr:housenumber"]
                         ? row["addr:housenumber"] + " "
                         : "";
@@ -107,7 +164,58 @@ app.post("/api/search", async function (req, res) {
         name += row["addr:city"] ? row["addr:city"] + ", " : "";
         name += row["addr:state"] ? row["addr:state"] + " " : "";
         name += row["addr:postcode"] ? row["addr:postcode"] : "";
+        if(name.length == 0) continue
+        if(curr["_source"]["geometry"]["type"] == "Point"){
+          results.push({
+              name: name,
+              coordinates: {
+                lat: coords[1],
+                lon: coords[0],
+              },
+              bbox: {
+                minLat: coords[1],
+                minLon: coords[0],
+                maxLat: coords[1],
+                maxLon: coords[0],
+              },
+            });
+          continue
+        }
+        var _minLat = 1000000000;
+        var _minLon = 1000000000;
+        var _maxLat = -10000000;
+        var _maxLon = -10000000;
+        var _centerLon = 0;
+        var _centerLat = 0;
+        for (var j = 0; j < coords.length; j++) {
+          var currPoint = coords[j];
+          _minLat = Math.min(parseFloat(currPoint[1]), _minLat);
+          _minLon = Math.min(parseFloat(currPoint[0]), _minLon);
+          _centerLon += parseFloat(currPoint[0]);
+          _centerLat += parseFloat(currPoint[1]);
+          _maxLat = Math.max(parseFloat(currPoint[1]), _maxLat);
+          _maxLon = Math.max(parseFloat(currPoint[0]), _maxLon);
+        }
+        _centerLat = _centerLat / coords.length;
+        _centerLon = _centerLon / coords.length;
+        results.push({
+          name: name,
+          coordinates: {
+            lat: _centerLat,
+            lon: _centerLon,
+          },
+          bbox: {
+            minLat: _minLat,
+            minLon: _minLon,
+            maxLat: _maxLat,
+            maxLon: _maxLon,
+          },
+        });
       }
+    for (var i = 0; i < rest.length; i++) {
+      const curr = rest[i];
+      var coords = curr["_source"]["geometry"]["coordinates"];
+      var  name = curr["_source"]["name"]
       if(curr["_source"]["geometry"]["type"] == "Point"){
         results.push({
             name: name,
@@ -155,6 +263,7 @@ app.post("/api/search", async function (req, res) {
         },
       });
     }
+   
    
     res.status(200).json(results);
   } catch (error) {
