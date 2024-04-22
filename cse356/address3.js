cdb9d6a2162e37b97b7c9e35de3a8416a23610b5
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-04-22 11:56:36 -0400

    Fix OR to AND

--- cse356/address3.js
+++ cse356/address3.js
@@ -1,29 +1,31 @@
-const express = require('express');
-const { Client } = require('pg');
+const express = require("express");
+const { Client } = require("pg");
 
 const app = express();
 
 // Configure PostgreSQL connection
 const client = new Client({
-  user: 'renderer',
-  host: '146.190.141.61',
-  database: 'gis',
-  password: 'renderer',
+  user: "renderer",
+  host: "146.190.141.61",
+  database: "gis",
+  password: "renderer",
   port: 5432, // Default PostgreSQL port
 });
 
 // Connect to the database
-client.connect()
+client
+  .connect()
   .then(() => {
-    console.log('Connected to PostgreSQL database');
+    console.log("Connected to PostgreSQL database");
   })
-  .catch(error => {
-    console.error('Error connecting to PostgreSQL database:', error);
+  .catch((error) => {
+    console.error("Error connecting to PostgreSQL database:", error);
   });
 
 app.use(express.json());
-app.post('/api/address', async (req, res) => {
-    const { lat, lon } = req.body;
+app.post("/api/address", async (req, res) => {
+  const { lat, lon } = req.body;
+  console.log(req.body);
   try {
     // Your PostGIS query
     const query = `
@@ -39,32 +41,34 @@ app.post('/api/address', async (req, res) => {
     planet_osm_point points
     ) AS p1
     WHERE (p1.tags->'addr:city' IS NOT NULL
-    OR p1.tags->'addr:unit' IS NOT NULL
-    OR p1.tags->'addr:state' IS NOT NULL
-    OR p1.tags->'addr:street' IS NOT NULL)
+    AND p1.tags->'addr:unit' IS NOT NULL
+    AND p1.tags->'addr:state' IS NOT NULL
+    AND p1.tags->'addr:street' IS NOT NULL)
     ORDER BY dist ASC
     LIMIT 10;
     `;
-    
+
     // Execute the query
     const result = await client.query(query);
-    const addr = result.rows[0]
+    console.log(result.rows);
+    const addr = result.rows[0];
     // Send query result as JSON response
-    res.status(200).json({
-        "number": addr["housenumber"] ? addr["housenumber"] : "",
-        "street": addr["street"] ? addr["street"] : "",
-        "city": addr["city"] ?  addr["city"]: "",
-        "state": addr["state"] ? addr["state"] : "", 
-        "country": "US"
-      })
+    var r = {
+      number: addr["housenumber"] ? addr["housenumber"] : "",
+      street: addr["street"] ? addr["street"] : "",
+      city: addr["city"] ? addr["city"] : "",
+      state: addr["state"] ? addr["state"] : "",
+      country: "US",
+    };
+    console.log(r);
+    res.status(200).json(r);
   } catch (error) {
     // Handle errors
-    console.error('Error executing query:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // Start the server
 
-app.listen(25000, () => {
-});
+app.listen(25000, () => {});
